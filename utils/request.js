00d828ccafe43a4889f4ebcb2053354ea6074480
DJwei <2900867051@qq.com>
2020-12-12 12:31:53 +0800

    拿不到AppDateshuju

---
+++ utils/request.js
@@ -0,0 +1,41 @@
+// 发送ajax请求
+/*
+*1.封装功能页面
+*   1.功能点明确
+    2.函数内部应该保留固定代码（静态的）
+    3.将动态的数据抽取成形参，由使用者根据自身情况动态的传入实参
+    4.一个良好的组件应该设置形参默认值（ES6的形参默认值
+*2.封装功能组件
+*   1.功能点明确
+    2.组件内部保留静态的代码
+    3.将动态的数据抽取成props参数，由使用者根据自身情况以标签属性的形式传入props数据
+    4.一个良好的组件应该设置组件的必要性及数据类型
+    prop:{
+      msg:{
+        required: ture,
+        default: 默认值.
+        type: String
+      }
+    }
+**/
+ 
+export default (url,data={},method='GET') => {
+  return new Promise((resolve,reject) => { 
+    //1. new Promise初始化promise实例的状态为pending
+   wx.request({
+   url    ,
+   data ,
+   method ,
+   
+  success: (res) =>{
+     console.log('请求成功:',res);
+      //resolve(res.data);
+  },
+  fail: (err) =>{
+     //console.log('请求失败:',err);
+     reject(err);
+  }
+   }) 
+ })
+  
+ }
